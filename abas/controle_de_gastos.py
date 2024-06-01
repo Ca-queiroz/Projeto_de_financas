45fd7239ff23ec1df3ad303d4e9a05d32954fdb8
Biibernardo <bianca.bernardo.ismart@gmail.com>
2024-06-01 17:41:18 -0300

    mudanças feitas

--- abas/controle_de_gastos.py
+++
@@ -1,7 +0,0 @@
-from tkinter import ttk
-
-def criar_aba2(notebook):
-    aba2 = ttk.Frame(notebook)
-    notebook.add(aba2, text='Controle de Gastos')
-    ttk.Label(aba2, text="Conteúdo da Aba 2", font=("Arial", 24)).pack(pady=50)
-    return aba2
