e323103fe61546c6168bcc175b920e82e4613f1b
Ca-queiroz <camila.rf.queiroz@outlook.com>
2024-06-02 18:36:14 -0300

    Ajustando Abas

--- abas/fluxo_de_finanças.py
+++ abas/fluxo_de_finanças.py
@@ -3,10 +3,5 @@ from tkinter import ttk, Frame
 def criar_aba1(notebook, bg_color):
     aba1 = Frame(notebook, bg=bg_color)  # Usando Frame do Tkinter para suportar a configuração de cor de fundo
     notebook.add(aba1, text='Fluxo de Finanças')
-<<<<<<< HEAD:abas/fluxo_de_financas.py
     ttk.Label(aba1, text="Conteúdo da Aba 1", font=("Arial", 24), background=bg_color).pack(pady=50)
     return aba1
-=======
-    ttk.Label(aba1, text="Conteúdo da Aba 1", font=("Arial", 24)).pack(pady=50)
-    return aba1
->>>>>>> main:abas/abas/fluxo_de_finanças.py
