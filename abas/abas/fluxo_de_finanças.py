29bbdced6aaacd74be2d4670903e79248a2943d2
fer0mendes <110433925+fer0mendes@users.noreply.github.com>
2024-06-05 19:54:54 -0300

    Update fluxo_de_finanças.py

--- abas/abas/fluxo_de_finanças.py
+++ abas/abas/fluxo_de_finanças.py
@@ -1,4 +1,4 @@
-from tkinter import Label
+from tkinter import Label, Entry
 import sys
 import numpy as np
 import tkinter as tk
@@ -7,6 +7,8 @@ from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkcalendar import Calendar, DateEntry
+from datetime import date
 
 co0 = "#2e2d2b"  # preta
 co1 = "#feffff"  # branca
@@ -89,8 +91,8 @@ label_imagem3.pack()
 #criando percentagem 
 
 def percentagem():
-  nome1 = Label(Framebaixo1, text='Percentagem da Receita Gasta', height=1, font=('Georgia 14'), bg = co2, fg=co4 )
-  nome1.place(x = 5 , y = 7)
+  nome1 = Label(Framebaixo1, text='Receita Gasta', height=1, font=('Georgia 14'), bg = co2, fg=co4 )
+  nome1.place(x = 20 , y = 7)
 
   style = ttk.Style()
   style.theme_use('default')
@@ -99,12 +101,12 @@ def percentagem():
 
 
   bar = ttk.Progressbar(Framebaixo1, length=180)
-  bar.place(x=10, y=35)
+  bar.place(x=25, y=35)
   bar['value'] = 50
 
   valor = 50
 
-  percentagem1 = Label(Framebaixo1, text='{:,.2f}'.format(valor), height=1, font=('Georgia 14'), bg = co2, fg=co4 )
+  percentagem1 = Label(Framebaixo1, text='{:,.2f} %'.format(valor), height=1, font=('Georgia 14'), bg = co2, fg=co4 )
   percentagem1.place(x = 200 , y = 35)
 
 #função para gráfico bars
@@ -145,7 +147,7 @@ def grafico_bar():
   ax.xaxis.grid(False)
 
   canva = FigureCanvasTkAgg(figura, Framebaixo1)
-  canva.get_tk_widget().place(x=10, y=70)
+  canva.get_tk_widget().place(x=25, y=90)
 
 # funcao de resumo
 
@@ -153,29 +155,63 @@ def resumo():
   valor = [500, 600, 420]
 
   l_linha = Label(Framebaixo1, text = '', width = 215, height = 1, font = ('Arial 1'), bg = '#545454')
-  l_linha.place(x=309, y = 52)
+  l_linha.place(x=350, y = 52)
   l_sumario = Label(Framebaixo1, text = 'Renda Mensal Total        '.upper(), font = ('Georgia 12'), bg = co2, fg = co4)
-  l_sumario.place(x=309, y = 35)
+  l_sumario.place(x=350, y = 35)
   l_sumario = Label(Framebaixo1, text = 'R$ {:,.2F}'.format(valor[0]), font = ('Arial 17'), bg = co2, fg = co4)
-  l_sumario.place(x=309, y = 70)
+  l_sumario.place(x=350, y = 70)
 
 
   l_linha = Label(Framebaixo1, text = '', width = 215, height = 1, font = ('Arial 1'), bg = '#545454')
-  l_linha.place(x=309, y = 132)
+  l_linha.place(x=350, y = 132)
   l_sumario = Label(Framebaixo1, text = 'Despesa Mensal Total       '.upper(), font = ('Georgia 12'), bg = co2, fg = co4)
-  l_sumario.place(x=309, y = 115)
+  l_sumario.place(x=350, y = 115)
   l_sumario = Label(Framebaixo1, text = 'R$ {:,.2F}'.format(valor[1]), font = ('Arial 17'), bg = co2, fg = co4)
-  l_sumario.place(x=309, y = 150)
+  l_sumario.place(x=350, y = 150)
 
 
   l_linha = Label(Framebaixo1, text = '', width = 215, height = 1, font = ('Arial 1'), bg = '#545454')
-  l_linha.place(x=309, y = 207)
+  l_linha.place(x=350, y = 207)
   l_sumario = Label(Framebaixo1, text = 'Saldo Total                          '.upper(), font = ('Georgia 12'), bg = co2, fg = co4)
-  l_sumario.place(x=309, y = 190)
+  l_sumario.place(x=350, y = 190)
   l_sumario = Label(Framebaixo1, text = 'R$ {:,.2F}'.format(valor[2]), font = ('Arial 17'), bg = co2, fg = co4)
-  l_sumario.place(x=309, y = 230)
+  l_sumario.place(x=350, y = 230)
 
-#aaa
+#configurações despesas
+
+info1 = Label(Framebaixo1, text = 'Insira novas despesas', height = 1, font=('Georgia 10 bold'), bg = co2, fg = co4)
+info1.place(x= 20, y = 320)
+
+#-------------------
+l_categoria = Label(Framebaixo1, text = 'Categoria', height = 1, font=('Ivy 10'), bg = co2, fg = co4)
+l_categoria.place(x= 20, y = 350)
+
+categoria_funcao = ['Viagem','Comida']
+categoria = []
+
+for i in categoria_funcao:
+  categoria.append(i[1])
+
+combo_categoria_despesas = ttk.Combobox(Framebaixo1, width=10, font=('Ivy 10'))
+combo_categoria_despesas['values'] = (categoria)
+combo_categoria_despesas.place(x = 120, y = 351)
+
+#-------------------
+cal_despesas = Label(Framebaixo1, text = 'Data', height = 1, font=('Ivy 10'), bg = co2, fg = co4)
+cal_despesas.place(x= 20, y = 380)
+
+e_cal_despesas = DateEntry(Framebaixo1, width = 12, background = 'darkgreen', foreground = 'white', borderwidth = 2, year = 2024)
+e_cal_despesas.place(x=120, y = 381)
+
+#------------------
+valor_despesas = Label(Framebaixo1, text = 'Quantia Total', height = 1, font=('Ivy 10'), bg = co2, fg = co4)
+valor_despesas.place(x= 20, y = 410)
+
+e_valor_despesas = Entry(Framebaixo1, width = 14, justify = 'left', relief = 'solid')
+e_valor_despesas.place(x=120, y = 411)
+
+
+
 
 
 percentagem()
